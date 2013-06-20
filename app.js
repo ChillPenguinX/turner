f2f359bb58d04def57b1b2f28f9518f918eb2b39
Andrew Brasuell <brasuell2004@yahoo.com>
2013-06-20 00:27:55 -0400

    TWEEEEEEETS

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ if ('development' == app.get('env')) {
 app.get('/', routes.index);
 app.get('/users', user.list);
 app.get('/auth/twitter', twitter.oauth);
-app.get('/auth/twitter/callback', twitter.callback);
+app.get('/cnnbrk-tweets', twitter.tweets);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
--- routes/twitter.js
+++ routes/twitter.js
@@ -2,55 +2,52 @@
  * tweeter stuffs
  */
 
-var OAuth= require('oauth').OAuth;
+var OAuth2= require('oauth').OAuth2;
+var $ = require('jquery');
 
-var oa = new OAuth(
-  "https://api.twitter.com/oauth/request_token",
-  "https://api.twitter.com/oauth/access_token",
-  "fVPjxzmmWfPgzIWeN5S2w",
-  "OmjGlv9ItlHGcuD1yVyH8NG5kgaGgriWZvigG1yK80",
-  "1.0",
-  "http://localhost:30000/auth/twitter/callback",
-  "HMAC-SHA1"
+var oauth2 = new OAuth2(
+  'fVPjxzmmWfPgzIWeN5S2w',
+  'OmjGlv9ItlHGcuD1yVyH8NG5kgaGgriWZvigG1yK80', 
+  'https://api.twitter.com/', 
+  null,
+  'oauth2/token', 
+  null
 );
 
 exports.oauth = function(req, res) {
-  oa.getOAuthRequestToken(function(error, oauth_token, oauth_token_secret, results){
-    if (error) {
-      console.log(error);
-      res.send("yeah no. didn't work.")
+  oauth2.getOAuthAccessToken(
+    '',
+    {'grant_type':'client_credentials'},
+    function (e, access_token, refresh_token, results){
+      // console.log('bearer: ',access_token);
+      // done();
+      req.session.access_token = access_token;
+      res.redirect("/cnnbrk-tweets");
     }
-    else {
-      req.session.oauth = {};
-      req.session.oauth.token = oauth_token;
-      console.log('oauth.token: ' + req.session.oauth.token);
-      req.session.oauth.token_secret = oauth_token_secret;
-      console.log('oauth.token_secret: ' + req.session.oauth.token_secret);
-      res.redirect('https://twitter.com/oauth/authenticate?oauth_token='+oauth_token)
-    }
-  });
+  );
 };
 
-exports.callback = function(req, res, next){
-  if (req.session.oauth) {
-    req.session.oauth.verifier = req.query.oauth_verifier;
-    var oauth = req.session.oauth;
-
-    oa.getOAuthAccessToken(oauth.token,oauth.token_secret,oauth.verifier, 
-    function(error, oauth_access_token, oauth_access_token_secret, results){
-      if (error){
-        console.log(error);
-        res.send("yeah something broke.");
-      } else {
-        req.session.oauth.access_token = oauth_access_token;
-        req.session.oauth,access_token_secret = oauth_access_token_secret;
-        console.log(results);
-        res.send("worked. nice one.");
+exports.tweets = function(req, res) {
+  if (req.session.access_token) {
+    // res.send(req.session.oauth);
+    $.ajax({
+      url: 'https://api.twitter.com/1.1/statuses/user_timeline.json', 
+      data: { 
+        screen_name: 'cnnbrk',
+        count: 10
+      },
+      headers: {
+        Authorization: 'Bearer ' + req.session.access_token
+      },
+      success: function(data, textStatus, jqXHR) {
+        res.render('tweets', { title: 'cnnbrk', tweets: data });
+      },
+      error: function(data) {
+        res.send(data);
       }
-    }
-    );
-  } 
-  else {
-    next(new Error("you're not supposed to be here."));
+    });
+  } else {
+    res.redirect('/auth/twitter');
   }
 }
+
